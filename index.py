a9e4b5919d95c53ff2aa9a329a94d5e77c05f691
benicio <gustavo024dias@gmail.com>
2023-04-20 19:36:25 -0300

    v 1.0 deploy

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from blueprints.CRUD import crud_bp
-from blueprints.RedLinks import redlinks_bp
+from api.blueprints.CRUD import crud_bp
+from api.blueprints.RedLinks import redlinks_bp
 
 app = Flask(__name__)
 app.register_blueprint(crud_bp)
