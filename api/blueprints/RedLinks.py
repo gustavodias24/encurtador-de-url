a70d887b3bc4baa35f99a7f81be1df2a1a6a82ba
benicio <gustavo024dias@gmail.com>
2023-04-20 19:27:45 -0300

    v 1.0 deploy

--- api/blueprints/RedLinks.py
+++ api/blueprints/RedLinks.py
@@ -5,13 +5,15 @@ import json
 redlinks_bp = Blueprint("redlinks_bp", __name__)
 
 
+@redlinks_bp.route("/", methods=["GET"])
+def default_page():
+    return jsonify({"msg": "Pagina em construcao..."})
+
+
 @redlinks_bp.route("/<string:alias>", methods=["GET"])
 def rediLink(alias):
-    if alias:
-        with open(os.path.join(os.getcwd(), "api", "blueprints", "raw-data", "data.json"), "r") as file:
-            for link in json.loads(file.read()):
-                if link["alias"] == alias:
-                    return redirect(link["redirecionar"])
-            return make_response(jsonify({"msg": "not found"}), 404)
-    else:
+    with open(os.path.join(os.getcwd(), "blueprints", "raw-data", "data.json"), "r") as file:
+        for link in json.loads(file.read()):
+            if link["alias"] == alias:
+                return redirect(link["redirecionar"])
         return make_response(jsonify({"msg": "not found"}), 404)
